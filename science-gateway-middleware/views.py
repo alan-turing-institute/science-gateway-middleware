f44087f51c5e3af9be1a040aa4c1ff527a9b97f9
Stuart Grieve <s.grieve@ucl.ac.uk>
2017-06-22 16:32:40 +0100

    tidying up code and commenting things

--- science-gateway-middleware/views.py
+++ science-gateway-middleware/views.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, abort, request, make_response
 from flask_httpauth import HTTPBasicAuth
-from flask import make_response
 from secrets import *
 
+# simple json, will be replaced by the incoming data from the frontend
 middle = [
     {
         'id': 1,
@@ -13,10 +13,11 @@ middle = [
 
 app = Flask(__name__)
 
-
+# initialise authentication
 auth = HTTPBasicAuth()
 
 
+# used to check a username and password against the values in secrets.py
 @auth.get_password
 def get_password(username):
     if username == USERNAME:
@@ -24,23 +25,29 @@ def get_password(username):
     return None
 
 
+# output an error message and 401 error code if user/pass are incorrect
 @auth.error_handler
 def unauthorized():
     return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 
+# basic homepage at top of domain, not needed in final app but good for testing
 @app.route("/")
 def hello():
     return "Hello World!"
 
 
-@app.route('/middle/api/v1.0/getter', methods=['GET'])
+# http get method to return contents of the json message, defined above
+@app.route('/middle/api/v1.0/get', methods=['GET'])
 @auth.login_required
 def get_information():
     return jsonify({'message': middle})
 
 
-#curl -i -H "Content-Type: application/json" -X POST -d '{"length":5}' http://localhost:5000/middle/api/v1.0/value
+# http post method to append data to the stored json.
+# to pass the json {"length":5} into the app use the following command:
+# curl -u <username>:<password> -i -H "Content-Type: application/json"
+# -X POST -d '{"length":5}' http://localhost:5000/middle/api/v1.0/value
 
 @app.route('/middle/api/v1.0/value', methods=['POST'])
 @auth.login_required
@@ -55,6 +62,7 @@ def recieve_information():
     return jsonify({'message': middle}), 201
 
 
+# handles 404 errors gracefully
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
