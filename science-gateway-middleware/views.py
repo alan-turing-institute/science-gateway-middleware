b11c0ac3c586922b6e80cba3eb88342574722402
Stuart Grieve <s.grieve@ucl.ac.uk>
2017-06-23 14:52:30 +0100

    MWE with post -> flask -> ssh

--- science-gateway-middleware/views.py
+++ science-gateway-middleware/views.py
@@ -1,15 +1,7 @@
 from flask import Flask, jsonify, abort, request, make_response
 from flask_httpauth import HTTPBasicAuth
+from ssh import ssh
 from secrets import *
-from simple import ssh
-
-# simple json, will be replaced by the incoming data from the frontend
-middle = [
-    {
-        'id': 1,
-        'length': 2,
-    }
-]
 
 
 app = Flask(__name__)
@@ -32,15 +24,10 @@ def unauthorized():
     return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 
-# basic homepage at top of domain, not needed in final app but good for testing
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-
-# ssh to a remote machine
-@app.route("/ssh")
-def ssh_attempt():
+def ssh_connect(command):
+    '''
+    Method to set up an ssh connection and send a command to the remote server.
+    '''
 
     # gathering the needed info from our secrets file
     username = SSH_USR
@@ -50,9 +37,6 @@ def ssh_attempt():
     else:
         port = 22
 
-    # making a simple command that multiplies 2 numbers
-    command = 'echo "$(({0} * {1}))"'.format(5, 10)
-
     connection = ssh(hostname, username, port)
     result = connection.pass_command(command)
     connection.close_connection()
@@ -60,35 +44,53 @@ def ssh_attempt():
     return result
 
 
-# http get method to return contents of the json message, defined above
-@app.route('/middle/api/v1.0/get', methods=['GET'])
-@auth.login_required
-def get_information():
-    return jsonify({'message': middle})
-
-
-# http post method to append data to the stored json.
-# to pass the json {"length":5} into the app use the following command:
-# curl -u <username>:<password> -i -H "Content-Type: application/json"
-# -X POST -d '{"length":5}' http://localhost:5000/middle/api/v1.0/value
-
-@app.route('/middle/api/v1.0/value', methods=['POST'])
+@app.route('/post', methods=['POST'])
 @auth.login_required
 def recieve_information():
-    if not request.json or 'length' not in request.json:
+    '''
+    http post method to append data to the stored json.
+    to pass the json {"length":5} into the app use the following command:
+    curl -u <username>:<password> -i -H "Content-Type: application/json"
+    -X POST -d '{"length":5, "width": 10}' http://localhost:5000/post
+    '''
+    if not request.json:
         abort(400)
-    tmp = {
-        'id': middle[-1]['id'] + 1,
+    if 'length' not in request.json and 'width' not in request.json:
+        abort(400)
+    input_data = {
         'length': request.json['length'],
+        'width': request.json['width'],
     }
-    middle.append(tmp)
-    return jsonify({'message': middle}), 201
+    result = workflow(input_data)
+    return result, 201
 
 
-# handles 404 errors gracefully
 @app.errorhandler(404)
 def not_found(error):
+    '''
+    handles 404 errors gracefully
+    '''
     return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+def workflow(input_data):
+    '''
+    Wrapper to chain the various methods together.
+    '''
+    command = build_command(input_data)
+    return ssh_connect(command)
+
+
+def build_command(input_data):
+    '''
+    Currently takes the input json data from the POST request and parses
+    it into our simple bash command. Eventually this method will parse the
+    frontend jsons and output commands for BEMPP
+    '''
+    # a simple command that multiplies 2 numbers
+    return 'echo "$(({0} * {1}))"'.format(input_data['length'],
+                                          input_data['width'])
+
 
 if __name__ == '__main__':
     app.run()
