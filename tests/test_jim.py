db51b412947aba6e3779901a1acfeb5bfa28bf55
Stuart Grieve <s.grieve@ucl.ac.uk>
2017-07-24 16:16:07 +0100

    test of constructor works locally if secrets are manually inputted

--- tests/test_jim.py
+++ tests/test_jim.py
@@ -1,10 +1,28 @@
 import pytest
 from middleware.job_information_manager import job_information_manager as JIM
-from werkzeug.test import EnvironBuilder
-from werkzeug.wrappers import Request
 
 
 class TestJIM(object):
     def test_constructor(self):
 
-        assert 1 == 1
+        # These variables should come from instance/config.py
+        username = ''
+        ssh_host = ''
+        port = 22
+
+        # Some testing data
+        simulation_root = 'path/to/test'
+        job = {'id': '1234', 'templates': 'test_template',
+               'scripts': 'test_script', 'parameters': 'viscosity'}
+
+        # Create a manager
+        jim = JIM(job, simulation_root)
+
+        assert jim.username == username
+        assert jim.hostname == ssh_host
+        assert jim.port == port
+        assert jim.simulation_root == simulation_root
+        assert jim.job_id == job['id']
+        assert jim.template_list == job['templates']
+        assert jim.parameter_patch == job['parameters']
+        assert jim.script_list == job['scripts']
