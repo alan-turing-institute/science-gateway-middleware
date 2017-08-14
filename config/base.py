4a02ac073780a9b32568fbbcc0fa3970dc626824
Martin O'Reilly <developer@mtspace.net>
2017-08-14 18:24:35 +0100

    Make Job API URL plural

--- config/base.py
+++ config/base.py
@@ -4,7 +4,7 @@
 cases_path = './resources/cases/blue.json'
 
 # A dictionary of URI stems for the various API actions
-URI_Stems = {'job': '/api/job/',
+URI_Stems = {'job': '/api/jobs/',
              'setup': '/api/setup/',
              'run': '/api/run/',
              'cancel': '/api/cancel/',
